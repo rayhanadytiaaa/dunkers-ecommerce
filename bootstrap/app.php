a54a185c7311405b9de24b7cf85612e55970bd7e
rayhanadytiaaa <rayhan.adytia11@gmail.com>
2026-07-14 22:44:31 +0700

    fix build style

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,12 +4,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
@@ -19,3 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+if ($app->environment('production')) {
+    $app['url']->forceRootUrl($app['config']->get('app.url'));
+    $app['url']->forceScheme('https');
+}
+
+return $app;
